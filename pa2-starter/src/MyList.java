92d4aaa981c15a2aa9adb452eaf223f0eb1e2b4b
Sophia Krause-Levy <skrauselevy@gmail.com>
2022-07-01 13:44:02 -0700

    Good luck!

--- pa2-starter/src/MyList.java
+++ pa2-starter/src/MyList.java
@@ -1,7 +1,7 @@
 
 public interface MyList<E> {
 	  E[] toArray();
-	  void transformAll(MyTransformer mt);
-	  void chooseAll(MyChooser mc);
+	  void transformAll(MyTransformer<E> mt);
+	  void chooseAll(MyChooser<E> mc);
 	  boolean isEmpty();
 }
